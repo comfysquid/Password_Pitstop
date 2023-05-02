b4faccbd4401331304a3d8d74bd5fb6cae4d052c
comfysquid <105414146+comfysquid@users.noreply.github.com>
2023-05-02 17:35:54 +1000

    Added logo & readme

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,14 +1,18 @@
 import { Link } from 'react-router-dom';
 import '../styles/navbar.css';
+import logo from '../styles/logo.png';
 
 function Navbar() {
   return (
     <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/generator">Password Generator</Link></li>
-        <li><Link to="/tester">Password Tester</Link></li>
-      </ul>
+      <div className="nav-container">
+        <img src={logo} alt="Logo" className="logo"/>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/generator">Password Generator</Link></li>
+          <li><Link to="/tester">Password Tester</Link></li>
+        </ul>
+      </div>
     </nav>
   );
 }
